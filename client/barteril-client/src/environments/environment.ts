f3ab67cd90e08b3d960d9b82ecaea5504063c8da
Oz Zorany <ozzorany@gmail.com>
2021-03-06 10:40:09 +0200

    firebase config

--- client/barteril-client/src/environments/environment.ts
+++ client/barteril-client/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'AIzaSyBzQeNQNSI7QsIUcSwCneF8N-dybSGWNXs',
+  authDomain: 'barteril.firebaseapp.com',
+  databaseURL: 'https://barteril-default-rtdb.firebaseio.com',
+  projectId: 'barteril',
+  storageBucket: 'barteril.appspot.com',
+  messagingSenderId: '257300103440',
+  appId: '1:257300103440:web:02078eb8be326777ace595',
+  measurementId: 'G-ZLF0G01PKB'
 };
 
 /*
