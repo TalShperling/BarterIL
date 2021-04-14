6928b3cb9042e3377b4a593aa616a14796ec9d7a
LiorVais <larrymv@gmail.com>
2021-04-14 13:53:07 +0300

    lint

--- src/app/items/services/categories.service.ts
+++ src/app/items/services/categories.service.ts
@@ -6,22 +6,25 @@ import { IFirebaseService } from '../../services/firebase/models/firebase-servic
 import { Category } from '../../../entities/category.model';
 
 @Injectable()
-export class CategoriesService implements IFirebaseService<Category>{
+export class CategoriesService implements IFirebaseService<Category> {
   collectionName = CollectionType.CATEGORIES;
 
-  constructor(private firebaseService: FirebaseService) {}
+  constructor(private firebaseService: FirebaseService) {
+  }
 
   getAll$(): Observable<Category[]> {
     return this.firebaseService.getAllData$<Category>(this.collectionName);
   }
+
   getById$(id: string): Observable<Category> {
     return this.firebaseService.getDataById$<Category>(this.collectionName, id);
   }
+
   upsert$(data: Category): Observable<Category> {
     return this.firebaseService.upsertDocument<Category>(this.collectionName, data);
   }
+
   delete$(data: Category): Observable<void> {
     return this.firebaseService.deleteDocument<Category>(this.collectionName, data);
   }
-
 }
