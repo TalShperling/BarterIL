277eec58d07b0ef154234b4a7651f26469351467
Oz Zorany <ozzorany@gmail.com>
2021-05-08 11:57:35 +0300

    add takeUntil

--- src/app/user/components/user-card/user-card/user-card.component.ts
+++ src/app/user/components/user-card/user-card/user-card.component.ts
@@ -5,18 +5,21 @@ import {Store} from '@ngrx/store';
 import {ItemsState} from '../../../../items/reducers/items.reducer';
 import {updateSuperficialData} from '../../../actions/user.actions';
 import {UserEditByAdminModalComponent} from '../../user-edit-by-admin-modal/user-edit-by-admin-modal.component';
+import {ObservableListener} from '../../../../components/observable-listener';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.scss']
 })
-export class UserCardComponent implements OnInit {
+export class UserCardComponent extends ObservableListener implements OnInit {
   @Input() user: User;
   modalRef: MDBModalRef;
 
   constructor(private store$: Store<ItemsState>,
               private modalService: MDBModalService) {
+    super();
   }
 
   ngOnInit(): void {
@@ -24,9 +27,10 @@ export class UserCardComponent implements OnInit {
 
   editUser(user: User): void {
     this.modalRef = this.modalService.show(UserEditByAdminModalComponent, {data: {user}});
-    this.modalRef.content.editUserEmitter.subscribe((editedUser: User) => {
-      this.store$.dispatch(updateSuperficialData({user: editedUser}));
-      this.modalRef.hide();
-    });
+    this.modalRef.content.editUserEmitter.pipe(takeUntil(this.unsubscribeOnDestroy))
+      .subscribe((editedUser: User) => {
+        this.store$.dispatch(updateSuperficialData({user: editedUser}));
+        this.modalRef.hide();
+      });
   }
 }
