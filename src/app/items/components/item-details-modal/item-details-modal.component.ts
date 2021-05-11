9c8b25284665fe88957f8dde88bab25cf4fe9896
TalShperling <talshper@gmail.com>
2021-05-12 00:21:58 +0300

    resolving conflicts

--- src/app/items/components/item-details-modal/item-details-modal.component.ts
+++ src/app/items/components/item-details-modal/item-details-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MDBModalRef } from 'angular-bootstrap-md';
 import { Item } from '../../../../entities/item.model';
 
@@ -7,10 +7,19 @@ import { Item } from '../../../../entities/item.model';
   templateUrl: './item-details-modal.component.html',
   styleUrls: ['./item-details-modal.component.scss']
 })
-export class ItemDetailsModalComponent {
+export class ItemDetailsModalComponent implements OnInit{
   item: Item;
+  imageSrc: string;
 
   constructor(public modalRef: MDBModalRef) {
+  }
+
+  ngOnInit(): void {
+    if(this.item.pictureUrls.length === 0) {
+      this.imageSrc = 'assets/images/no-image-to-show.png';
+    } else {
+      this.imageSrc = this.item.pictureUrls[0];
+    }
   }
 
   openOffer(): void {
