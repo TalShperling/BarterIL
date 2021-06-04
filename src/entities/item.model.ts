cc7365849b017129e418b7170176b4802ba6df98
yariv <yarivgavriel2@gmail.com>
2021-06-04 18:08:40 +0300

    remove item amount

--- src/entities/item.model.ts
+++ src/entities/item.model.ts
@@ -3,7 +3,6 @@ import { Category } from './category.model';
 export interface Item {
     id: string;
     name: string;
-    amount?: number;
     categories: Category[];
     description: string;
     ownerId: string;
