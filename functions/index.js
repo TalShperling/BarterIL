a4b5de0f1b034be7151911540b44be368b332ce9
TalShperling <talshper@gmail.com>
2021-06-07 23:57:28 +0300

    working onCreate

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const twilio = require('twilio');
+admin.initializeApp();
+
+exports.sendNotifications1 = functions.firestore.document('/transactions/{transactionId}').onCreate((snapshot, context) => {
+    console.log("Got response");
+    functions.logger.log("Hey");
+    const original = snapshot.data();
+    const accountSid = 'AC7aa4ad969f9cd63cc83c7e2e25616bed';
+    const authToken = '38a5d1fba91bea373bc3b100c61f3038';
+    const client = new twilio(accountSid, authToken);
+
+    console.log(original);
+
+   return client.messages.create({
+        from: '+17085017158',
+        body: "Hey",
+        to: '+972504084477'
+    })
+        .then((message) => {
+            console.log(message.sid);
+            console.log('Uppercasing', context.params.transactionId, original);
+            const uppercase = original.toUpperCase();
+            return snapshot.ref.parent.child('firstName').set(uppercase);
+        })
+        .catch((err) => {
+            throw (err);
+        });
+
+});
